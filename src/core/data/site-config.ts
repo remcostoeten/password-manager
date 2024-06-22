6d7a58c3f318e7a15e3dfcd2b0d5aa419210ac33
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-22 16:59:23 +0200

    chore: Add basic shadcn ui parts needed

---
+++ src/core/data/site-config.ts
@@ -0,0 +1,12 @@
+
+const siteConfig = {
+  title: "Password Manager",
+  description:
+    "A simple password manager built with React and Tailwind CSS with no backend. Password's are stored in local storage and not hashed/encrypted.",
+  name: "Remco Stoeten",
+  github: "https://github.com/remcostoeten",
+  "deploy-url": "https://password-manager.remcostoeten.com",
+  repository: "https://github.com/remcostoeten/password-manager",
+};
+
+export default siteConfig;
