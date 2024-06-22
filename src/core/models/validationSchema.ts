379a8af047a1fa2eabb0e931cbdd33c9839b1f4a
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-22 23:51:29 +0200

    add zod validation schema

---
+++ src/core/models/validationSchema.ts
@@ -0,0 +1,9 @@
+import * as z from "zod";
+
+export const passwordSchema = z.object({
+  website: z.string().min(1, "Website is required"),
+  username: z.string().min(1, "Username is required"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
+export type PasswordFormData = z.infer<typeof passwordSchema>;
