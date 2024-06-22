c5c119ab434d802a548d5fd41ceb9fb5d8a43712
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-22 22:31:42 +0200

    removed container because looks nicer

--- src/components/shells/PageWrapper.tsx
+++ src/components/shells/PageWrapper.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react';
 export default function PageWrapper({ children }: { children: ReactNode }) {
   return (
     <main className="dark bg-background text-foreground min-h-screen flex flex-col">
-      <div className="container">{children}</div>
+      {children}
     </main>
   );
 }
