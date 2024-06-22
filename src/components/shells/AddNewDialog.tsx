1cbe99d6d3bba1499f5b9d9ae63ca73c25366ecd
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-23 01:08:31 +0200

    feat: Add vault entrys to locale storage

--- src/components/shells/AddNewDialog.tsx
+++ src/components/shells/AddNewDialog.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  passwordSchema,
+  PasswordFormData,
+} from '../../core/models/validationSchema';
 import {
   Button,
   Dialog,
@@ -9,73 +17,92 @@ import {
   DialogTrigger,
   Input,
   Label,
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '../ui';
+} from '@/components/ui';
+import { toast } from 'sonner';
+import { useLocalStorage } from '@/core/hooks/useLocalStorage';
 
 export default function AddNewDialog() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PasswordFormData>({
+    resolver: zodResolver(passwordSchema),
+  });
+
+  const [vaultEntries, setVaultEntries] = useLocalStorage<PasswordFormData[]>(
+    'vault-entries',
+    [],
+  );
+
+  const onSubmit = (data: PasswordFormData) => {
+    setVaultEntries((prevEntries) => [...prevEntries, data]);
+    toast('Entry saved successfully to the vault!');
+  };
+
+  const onError = (errors: any) => {
+    console.log('Form errors:', errors);
+    toast('Error saving entry to the vault!');
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button variant="outline">Create New Password</Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] darkbg-background dark:text-foreground">
-        <DialogHeader>
-          <DialogTitle>Create New Password</DialogTitle>
-          <DialogDescription>
-            Enter the details for your new password.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="website" className="text-right">
-              Website
-            </Label>
-            <Input
-              id="website"
-              placeholder="Enter website name"
-              className="col-span-3"
-            />
+      <DialogContent className="sm:max-w-[425px] dark:bg-background dark:text-foreground">
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
+          <DialogHeader>
+            <DialogTitle>Create New Password</DialogTitle>
+            <DialogDescription>
+              Enter the details for your new password.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div>
+              <Label htmlFor="website" className="text-right">
+                Website
+              </Label>
+              <Input
+                id="website"
+                {...register('website')}
+                className="col-span-3"
+              />
+              {errors.website && <span>{errors.website.message}</span>}
+            </div>
+            <div>
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input
+                id="username"
+                {...register('username')}
+                className="col-span-3"
+              />
+              {errors.username && <span>{errors.username.message}</span>}
+            </div>
+            <div>
+              <Label htmlFor="password" className="text-right">
+                Password
+              </Label>
+              <Input
+                id="password"
+                type="password"
+                {...register('password')}
+                className="col-span-3"
+              />
+              {errors.password && <span>{errors.password.message}</span>}
+            </div>
           </div>
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="password" className="text-right">
-              Password
-            </Label>
-            <Input
-              id="password"
-              type="password"
-              placeholder="Enter password"
-              className="col-span-3"
-            />
-          </div>
-          <div className="grid items-center grid-cols-4 gap-4">
-            <Label htmlFor="company" className="text-right">
-              Company
-            </Label>
-            <Select>
-              <SelectTrigger className="col-span-3">
-                <SelectValue placeholder="Select company" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="google">Google</SelectItem>
-                <SelectItem value="amazon">Amazon</SelectItem>
-                <SelectItem value="microsoft">Microsoft</SelectItem>
-                <SelectItem value="apple">Apple</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-        <DialogFooter>
-          <Button
-            type="submit"
-            className="dark:bg-primary dark:text-primary-foreground"
-          >
-            Save Password
-          </Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button
+              type="submit"
+              className="dark:bg-primary dark:text-primary-foreground"
+            >
+              Save Password
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
